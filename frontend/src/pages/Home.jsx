b4e289b79a27e4a84eebef0431c4e220e4c3c23a
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2022-10-17 00:51:40 +0530

    initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<div>
+			<div>
+				<h1>The Best Things In Life Are Free</h1>
+			</div>
+			<div>
+				<button>Learn more</button>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
