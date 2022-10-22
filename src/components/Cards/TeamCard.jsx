74e0b4401467897dcd56a8e4549e41c450144788
HarshJa1n <hjain012003@gmail.com>
2022-10-22 12:05:50 +0530

    TeamCard: Shadow Added

--- src/components/Cards/TeamCard.jsx
+++ src/components/Cards/TeamCard.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const TeamCard = (props) => {
   const { member } = props;
   return (
-    <div className="team-card flex-col">
-      <div className="tc-inner-wrapper m-5 bg-gradient-to-r from-cyan-400 to-fuchsia-600 p-[3px] rounded-lg">
+    <div className="team-card flex-col ">
+      <div className="tc-inner-wrapper m-5 bg-gradient-to-r from-cyan-400 to-fuchsia-600 p-[3px] rounded-lg  shadow-xl shadow-gray-700">
         <div className="tc-content bg-bgblue rounded-lg p-2 ">
           <img
             alt={member.name}
             src={member.image}
-            className="tc-banner w-40 rounded-lg  "
+            className="tc-image w-40 rounded-lg  "
           />
-          <div className="tc-user-info flex flex-col justify-center items-center w-full mt-2 ">
+          <div className="tc-info flex flex-col justify-center items-center w-full mt-2 ">
             <div className="text-twhite font-audiowide">{member.name}</div>
             <div className="text-slate-400 text-xs italic ">{member.role}</div>
           </div>
