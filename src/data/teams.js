557e5066fb4931a024df7a236954b44244447fad
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-02-26 18:58:28 +0530

    update data

--- src/data/teams.js
+++ src/data/teams.js
@@ -3,49 +3,98 @@ export const TEAM_MEMEBERS = [
     id: 1,
     image:
       "https://raw.githubusercontent.com/HarshJa1n/harshfolio/master/src/assets/my.jpg",
-    name: "Harsh Jain",
+    name: "Chitransh Kulshrestha",
     role: "Asst Web Developer",
   },
   {
     id: 2,
     image:
       "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
-    name: "Jayant Singh",
+    name: "Harsh Jain",
     role: "Asst Web Developer",
   },
   {
     id: 3,
     image:
       "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
-    name: "Aayush Rathore",
-    role: "Core team member",
-  },
-  {
-    id: 3,
-    image:
-      "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
-    name: "Aayush Rathore",
+    name: "Rajat Kumawat",
     role: "Core team member",
   },
   {
     id: 4,
     image:
       "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
-    name: "Aayush Rathore",
+    name: "Saloni Chaudhary",
     role: "Core team member",
   },
   {
     id: 5,
     image:
       "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
-    name: "Aayush Rathore",
+    name: "Amay shah",
     role: "Core team member",
   },
   {
     id: 6,
     image:
       "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
-    name: "Aayush Rathore",
+    name: "Jayant Singh",
+    role: "Core team member",
+  },
+  {
+    id: 7,
+    image:
+      "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
+    name: "Sahil Phulwani",
+    role: "Core team member",
+  }, 
+  {
+    id: 8,
+    image:
+      "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
+    name: "Jiya Rathore",
+    role: "Core team member",
+  }, 
+  {
+    id: 9,
+    image:
+      "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
+    name: "Arya Sharma",
+    role: "Core team member",
+  },
+  {
+    id: 10,
+    image:
+      "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
+    name: "Satyam Sagar",
+    role: "Core team member",
+  },
+  {
+    id: 11,
+    image:
+      "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
+    name: "Shreem Asati",
+    role: "Core team member",
+  },
+  {
+    id: 12,
+    image:
+      "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
+    name: "Shailly Kumar",
+    role: "Core team member",
+  },
+  {
+    id: 13,
+    image:
+      "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
+    name: "Pratham Saraf",
+    role: "Core team member",
+  },
+  {
+    id: 14,
+    image:
+      "https://thumbs.dreamstime.com/b/creative-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mockup-144847501.jpg",
+    name: "Manasvi Singh",
     role: "Core team member",
   },
 ];
@@ -89,6 +138,6 @@ export const PRESIDENTS = [
     name: "Jayant Singh",
     role: "Asst Web Developer",
   },
- 
+
 
 ];
